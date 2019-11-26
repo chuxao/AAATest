b0105b7c9fe407486df46ee6783d36168e3e6263
chuxiao <xchu@i9i8.com>
2019-11-26 20:17:50 +0800

    Initial Commit of Library
    Signed-off-by: chuxiao <xchu@i9i8.com>

---
+++ Example/Pods/Target Support Files/AAATest/AAATest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AAATest : NSObject
+@end
+@implementation PodsDummy_AAATest
+@end
---
+++ Example/Pods/Target Support Files/Pods-AAATest_Example/Pods-AAATest_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AAATest_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AAATest_Example
+@end
